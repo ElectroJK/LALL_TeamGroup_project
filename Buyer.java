38a14eaabc9f7bf7136ea115ba7c5f9a0cc75756
Мейіржан Жұмабек <meiirzhanzhumabek@MacBook-Air-Mejirzan.local>
2024-10-06 01:04:39 +0500

    Here was Meiirzhan

--- Buyer.java
+++ Buyer.java
@@ -2,19 +2,15 @@ interface Buyer {
   boolean CanAfford(iPhone phone);
   void purchase(iPhone phone);
 }
-
 class iPhoneBuyer implements Buyer {
   private Person person;
-
   public iPhoneBuyer(Person person) {
     this.person = person;
   }
-
   @Override
   public boolean CanAfford(iPhone phone) {
     return person.getBudget() >= phone.getPrice();
   }
-
   @Override
   public void purchase(iPhone phone) {
     if (CanAfford(phone)) {
--- Store.java
+++ Store.java
@@ -1,10 +1,15 @@
 class Store {
+  private static Store instance;
   private Buyer buyer;
-
-  public Store(Buyer buyer) {
+  private Store(Buyer buyer) {
     this.buyer = buyer;
   }
-
+  public static Store getInstance(Buyer buyer) {
+    if (instance == null) {
+      instance = new Store(buyer);
+    }
+    return instance;
+  }
   public void attemptPurchase(Customer customer) {
     iPhone phone = customer.getPhone();
     buyer.purchase(phone);
